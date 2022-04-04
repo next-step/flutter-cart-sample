53ec264dad72d3b126aa73fe6317849e72e3f490
youngbin jang <youngbin.jang@navercorp.com>
2022-04-04 15:58:10 +0900

    add backgroundColor

--- lib/cart_screen.dart
+++ lib/cart_screen.dart
@@ -11,6 +11,7 @@ class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromRGBO(246, 246, 246, 1.0),
       appBar: AppBar(
         leading: const BackButton(
           color: Colors.black,
