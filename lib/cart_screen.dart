73afdc10a946346178c56d9fca622469ef18c6c0
youngbin jang <youngbin.jang@navercorp.com>
2022-04-04 16:26:29 +0900

    add menu

--- lib/cart_screen.dart
+++ lib/cart_screen.dart
@@ -34,7 +34,7 @@ class _CartScreenState extends State<CartScreen> {
           SizedBox(
             height: 1,
           ),
-
+          _buildMenu(),
         ],
       ),
     );
@@ -64,6 +64,81 @@ class _CartScreenState extends State<CartScreen> {
             '치킨 잠실점',
             style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
           )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildMenu() {
+    return Container(
+      color: Colors.white,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              SizedBox(
+                width: 20,
+              ),
+              Text(
+                '후라이드 치킨',
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Spacer(),
+              IconButton(
+                icon: Icon(
+                  Icons.close,
+                  color: Colors.grey,
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 20,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey.withOpacity(0.3),
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.asset(
+                    'images/chicken.png',
+                    width: 70,
+                    height: 70,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '• 찜 & 리뷰 약속 : 참여. 서비스음료제공',
+                    style: TextStyle(
+                      color: Color.fromRGBO(125, 125, 125, 1.0),
+                    ),
+                  ),
+                  Text('18,000원'),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
         ],
       ),
     );
