2e7e1057c4234d6c4786d29bbbc84b7b2a93dc8b
jyb <jyb0823@gmail.com>
2022-07-14 11:14:32 +0900

    store name widget 추가

---
+++ lib/widget/store_name.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class StoreNameWidget extends StatelessWidget {
+  const StoreNameWidget({
+    Key? key,
+    required this.name,
+    required this.imageUrl,
+  }) : super(key: key);
+
+  final String name;
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      height: 70,
+      child: Row(
+        children: [
+          SizedBox(
+            width: 20,
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Image.asset(
+              imageUrl,
+              width: 35,
+              height: 35,
+            ),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Text(
+            name,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
