b624cb14e6ebab9db4295a6d483aa6e50d7a7bc9
devsoupe <luvyum4e@gmail.com>
2022-08-25 23:07:24 +0900

    feat: bottomNavigationBar에 구현된 Container를 위젯으로 변경

--- lib/cart/cart_screen.dart
+++ lib/cart/cart_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cart_sample/cart/widget/add_more_widget.dart';
 import 'package:cart_sample/cart/widget/billing_widget.dart';
 import 'package:cart_sample/cart/widget/menu_widget.dart';
+import 'package:cart_sample/cart/widget/order_widget.dart';
 import 'package:cart_sample/cart/widget/store_name_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -46,58 +47,7 @@ class _CartScreenState extends State<CartScreen> {
           BillingWidget(),
         ],
       ),
-      bottomNavigationBar: Container(
-        color: Colors.white,
-        child: SafeArea(
-          child: Container(
-            height: 65,
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 10,
-            ),
-            child: ElevatedButton(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 25,
-                    height: 25,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      shape: BoxShape.circle,
-                    ),
-                    child: Center(
-                      child: Text(
-                        '1',
-                        style: TextStyle(
-                          color: Color.fromRGBO(44, 191, 188, 1.0),
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 7,
-                  ),
-                  Text(
-                    '21,000원 배달 주문하기',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(
-                  Color.fromRGBO(44, 191, 188, 1.0),
-                ),
-              ),
-              onPressed: () {},
-            ),
-          ),
-        ),
-      ),
+      bottomNavigationBar: OrderWidget(),
     );
   }
 }
