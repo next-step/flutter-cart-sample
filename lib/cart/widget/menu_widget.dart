7a2fd2625428a455074c0823e4c31016a233a9ce
devsoupe <luvyum4e@gmail.com>
2022-08-28 21:26:25 +0900

    feat: MenuWidget 수량 변경 버튼 추가

--- lib/cart/widget/menu_widget.dart
+++ lib/cart/widget/menu_widget.dart
@@ -69,6 +69,46 @@ class MenuWidget extends StatelessWidget {
             ],
           ),
           SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              _buildCount(),
+              SizedBox(
+                width: 20,
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCount() {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.withOpacity(0.4)),
+        borderRadius: BorderRadius.circular(6),
+      ),
+      child: Wrap(
+        crossAxisAlignment: WrapCrossAlignment.center,
+        children: [
+          IconButton(
+            icon: Icon(Icons.remove),
+            disabledColor: Colors.grey,
+            onPressed: () {
+              debugPrint('-');
+            },
+          ),
+          Text('1'),
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              debugPrint('+');
+            },
+          ),
         ],
       ),
     );
