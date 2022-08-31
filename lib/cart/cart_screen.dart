76205f85ffebf26943eac2e4ca0a583b545b2343
LeeDongGyu <brainbackdoor@gmail.com>
2022-08-31 09:54:01 +0900

    2단계 InheritedWidget 사용하기 (#7)
    * feat: plus 버튼 기능 추가
    
    * feat: minus 버튼 기능 추가
    
    * feat: 주문하기 버튼 클릭시 갯수 변경 반영
    
    * refactor: Count class rename

--- lib/cart/cart_screen.dart
+++ lib/cart/cart_screen.dart
@@ -3,10 +3,36 @@ import 'package:flutter/material.dart';
 import '../utils/format.dart';
 
 part './more.dart';
+
 part './order.dart';
+
 part './payment_amount.dart';
+
 part './product.dart';
+
 part './store_name.dart';
+
+class Count extends InheritedWidget with ChangeNotifier {
+  final int count;
+
+  Count({
+    Key? key,
+    this.count = 1,
+    required Widget child,
+  }) : super(key: key, child: child);
+
+  static Count of(BuildContext context) {
+    final Count? result =
+        context.dependOnInheritedWidgetOfExactType<Count>();
+    assert(result != null, 'No MenuCount found in context');
+    return result!;
+  }
+
+  @override
+  bool updateShouldNotify(Count oldWidget) {
+    return oldWidget.count != count;
+  }
+}
 
 class CartScreen extends StatefulWidget {
   const CartScreen({Key? key}) : super(key: key);
@@ -16,34 +42,84 @@ class CartScreen extends StatefulWidget {
 }
 
 class _CartScreenState extends State<CartScreen> {
+  var _count = 1;
+
   @override
   Widget build(BuildContext context) {
-    var paymentAmount = PaymentAmount(totalOrderAmount: 18000, deliveryTip: 3000);
-    return Scaffold(
-      backgroundColor: const Color.fromRGBO(246, 246, 246, 1.0),
-      appBar: _appBar(),
-      body: ListView(
+    var price = 18000;
+    var paymentAmount =
+        PaymentAmount(totalOrderAmount: price * _count, deliveryTip: 3000);
+    return Count(
+      count: _count,
+      child: Scaffold(
+        backgroundColor: const Color.fromRGBO(246, 246, 246, 1.0),
+        appBar: _appBar(),
+        body: ListView(
+          children: [
+            SizedBox(
+              height: 10,
+            ),
+            StoreName(name: '치킨 잠실점', image: 'images/chickenCartoonImage.jpg'),
+            SizedBox(
+              height: 1,
+            ),
+            Product(
+                name: '후라이드 치킨',
+                image: 'images/chicken.png',
+                contents: '• 찜 & 리뷰 약속 : 참여. 서비스음료제공',
+                price: price),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                _buildCount(),
+                SizedBox(
+                  width: 20,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 1,
+            ),
+            More(),
+            paymentAmount,
+          ],
+        ),
+        bottomNavigationBar: _bottomNavigationBar(paymentAmount.calculate()),
+      ),
+    );
+  }
+
+  Widget _buildCount() {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.withOpacity(0.4)),
+        borderRadius: BorderRadius.circular(6),
+      ),
+      child: Wrap(
+        crossAxisAlignment: WrapCrossAlignment.center,
         children: [
-          SizedBox(
-            height: 10,
+          IconButton(
+            icon: Icon(Icons.remove),
+            disabledColor: Colors.grey,
+            onPressed: _count == 1
+                ? null
+                : () {
+                    setState(() {
+                      _count--;
+                    });
+                  },
           ),
-          StoreName(name: '치킨 잠실점', image: 'images/chickenCartoonImage.jpg'),
-          SizedBox(
-            height: 1,
+          Text('$_count'),
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              setState(() {
+                _count++;
+              });
+            },
           ),
-          Product(
-              name: '후라이드 치킨',
-              image: 'images/chicken.png',
-              contents: '• 찜 & 리뷰 약속 : 참여. 서비스음료제공',
-              price: 18000),
-          SizedBox(
-            height: 1,
-          ),
-          More(),
-          paymentAmount,
         ],
       ),
-      bottomNavigationBar: _bottomNavigationBar(paymentAmount.calculate()),
     );
   }
 
