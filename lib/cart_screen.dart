1fd8ba7ebf7cb62d0801dc0bcd885a2759c599ef
jambottle <jaewon.walks@gmail.com>
2022-11-04 02:25:16 +0900

    refactor: Add PaymentInfo widget to replace _buildBilling method

--- lib/cart_screen.dart
+++ lib/cart_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/payment_info.dart';
 import 'widgets/product_card.dart';
 import 'widgets/product_more.dart';
 import 'widgets/shop_profile.dart';
@@ -43,7 +44,10 @@ class _CartScreenState extends State<CartScreen> {
             height: 1,
           ),
           ProductMore(),
-          _buildBilling(),
+          SizedBox(
+            height: 10,
+          ),
+          PaymentInfo(),
         ],
       ),
       bottomNavigationBar: Container(
@@ -97,91 +101,6 @@ class _CartScreenState extends State<CartScreen> {
             ),
           ),
         ),
-      ),
-    );
-  }
-
-  Widget _buildBilling() {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border(
-          bottom: BorderSide(
-            color: Colors.grey.withOpacity(0.3),
-            width: 2,
-          ),
-        ),
-      ),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              children: [
-                Text('총 주문금액'),
-                Spacer(),
-                Text('18,000원'),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              children: [
-                Text(
-                  '배탈팁',
-                  style: TextStyle(
-                    fontSize: 16,
-                  ),
-                ),
-                Spacer(),
-                Text(
-                  '3,000원',
-                  style: TextStyle(
-                    fontSize: 16,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: Divider(
-              color: Colors.grey,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              children: [
-                Text(
-                  '결제예정금액',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Spacer(),
-                Text(
-                  '21,000원',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-        ],
       ),
     );
   }
