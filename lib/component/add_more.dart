0ee3dbf1028f435ea17dd3211a18c0a0a8958a99
soojin roh <rsj3112@gmail.com>
2022-11-04 22:14:55 +0900

    [step1] 장바구니 화면의 helperMethod를 widget으로 리팩토링

---
+++ lib/component/add_more.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class AddMore extends StatefulWidget {
+  const AddMore({Key? key}) : super(key: key);
+
+  @override
+  State<AddMore> createState() => _AddMoreState();
+}
+
+class _AddMoreState extends State<AddMore> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            bottom: BorderSide(
+              color: Colors.grey.withOpacity(0.3),
+              width: 2,
+            ),
+          ),
+        ),
+        height: 50,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.add),
+            Text(
+              '더 담으러 가기',
+              style: TextStyle(fontSize: 17),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screen/cart_screen.dart
@@ -0,0 +1,104 @@
+import 'package:cart_sample/component/add_more.dart';
+import 'package:cart_sample/component/billing.dart';
+import 'package:cart_sample/component/menu.dart';
+import 'package:cart_sample/component/store_name.dart';
+import 'package:flutter/material.dart';
+
+class CartScreen extends StatefulWidget {
+  const CartScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CartScreen> createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(246, 246, 246, 1.0),
+      appBar: AppBar(
+        leading: const BackButton(
+          color: Colors.black,
+        ),
+        title: const Text(
+          '장바구니',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.white,
+      ),
+      body: ListView(
+        children: [
+          SizedBox(
+            height: 10,
+          ),
+          StoreName(),
+          SizedBox(
+            height: 1,
+          ),
+          Menu(),
+          SizedBox(
+            height: 1,
+          ),
+          AddMore(),
+          Billing(),
+        ],
+      ),
+      bottomNavigationBar: Container(
+        color: Colors.white,
+        child: SafeArea(
+          child: Container(
+            height: 65,
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 10,
+            ),
+            child: ElevatedButton(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 25,
+                    height: 25,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Center(
+                      child: Text(
+                        '1',
+                        style: TextStyle(
+                          color: Color.fromRGBO(44, 191, 188, 1.0),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 7,
+                  ),
+                  Text(
+                    '21,000원 배달 주문하기',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(
+                  Color.fromRGBO(44, 191, 188, 1.0),
+                ),
+              ),
+              onPressed: () {},
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+}
