b92c8fe36c734475b8eba1cd7178cd2779411634
jambottle <jaewon.walks@gmail.com>
2022-11-06 04:00:31 +0900

    feat: Update 2 data models (Shop/Product) - Change parameter type of getWon method from double to int

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -2,6 +2,12 @@ class Product {
   final String name;
   final String? description;
   final String? thumbnail;
+  final int originalPrice;
 
-  Product(this.name, {this.description, this.thumbnail});
+  Product(
+    this.name, {
+    this.description,
+    this.thumbnail,
+    required this.originalPrice,
+  });
 }
--- lib/utils/formatter.dart
+++ lib/utils/formatter.dart
@@ -1,8 +1,8 @@
 import 'package:intl/intl.dart';
 
 class Formatter {
-  static String getWon(double number) {
-    var f = NumberFormat.currency(
+  static String getWon(int number) {
+    final f = NumberFormat.currency(
       locale: 'ko_KR',
       customPattern: '###,###,###원',
     );
