0e225a85427bff86caf0fb77c0628669720ba1a4
jambottle <jaewon.walks@gmail.com>
2022-11-06 02:30:23 +0900

    feat: Add 2 data models (Shop/Product)

---
+++ lib/models/product.dart
@@ -0,0 +1,7 @@
+class Product {
+  final String name;
+  final String? description;
+  final String? thumbnail;
+
+  Product(this.name, {this.description, this.thumbnail});
+}
---
+++ lib/models/shop.dart
@@ -0,0 +1,6 @@
+class Shop {
+  final String name;
+  final String? thumbnail;
+
+  Shop(this.name, {this.thumbnail});
+}
