48937a94c7616136ceb075a27b469dc408feb9ab
baekseungyeol <beck33333@naver.com>
2022-11-06 12:16:06 +0900

    widget 분리

---
+++ lib/cart/cart_screen.dart
@@ -0,0 +1,53 @@
+import 'package:cart_sample/cart/widget/add_more_widget.dart';
+import 'package:cart_sample/cart/widget/billing_widget.dart';
+import 'package:cart_sample/cart/widget/menu_widget.dart';
+import 'package:cart_sample/cart/widget/order_button_widget.dart';
+import 'package:cart_sample/cart/widget/store_name_widget.dart';
+import 'package:flutter/material.dart';
+
+class CartScreen extends StatefulWidget {
+  const CartScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CartScreen> createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(246, 246, 246, 1.0),
+      appBar: AppBar(
+        leading: const BackButton(
+          color: Colors.black,
+        ),
+        title: const Text(
+          '장바구니',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.white,
+      ),
+      body: ListView(
+        children: [
+          SizedBox(
+            height: 10,
+          ),
+          StoreNameWidget(),
+          SizedBox(
+            height: 1,
+          ),
+          MenuWidget(),
+          SizedBox(
+            height: 1,
+          ),
+          AddMoreWidget(),
+          BillingWidget(),
+        ],
+      ),
+      bottomNavigationBar: OrderButtonWidget(),
+    );
+  }
+}
---
+++ lib/cart/widget/billing_widget.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class BillingWidget extends StatelessWidget {
+  const BillingWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          bottom: BorderSide(
+            color: Colors.grey.withOpacity(0.3),
+            width: 2,
+          ),
+        ),
+      ),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              children: [
+                Text('총 주문금액'),
+                Spacer(),
+                Text('18,000원'),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              children: [
+                Text(
+                  '배탈팁',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                Spacer(),
+                Text(
+                  '3,000원',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Divider(
+              color: Colors.grey,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              children: [
+                Text(
+                  '결제예정금액',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Spacer(),
+                Text(
+                  '21,000원',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+        ],
+      ),
+    );
+  }
+}
