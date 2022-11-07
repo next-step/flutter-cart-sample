66b325458925a388a5366aa5e27d2e0b30ad5efd
baekseungyeol <beck33333@naver.com>
2022-11-07 21:53:01 +0900

    refactor: final 변경 - 리뷰 반영

--- lib/utils/number.dart
+++ lib/utils/number.dart
@@ -1,6 +1,6 @@
 import 'package:intl/intl.dart';
 
 String formatPrice(int price) {
-  var numberFormat = NumberFormat.currency(locale: 'ko_KR', symbol: '');
+  final numberFormat = NumberFormat.currency(locale: 'ko_KR', symbol: '');
   return numberFormat.format(price) + '원';
 }
