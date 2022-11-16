a7d92db0aee2a08ac39e919019d4f92d01d98adf
yongju <odetsuo@gmail.com>
2022-11-16 15:23:36 +0900

    refactor: 상품명 위젯 분리

--- lib/cart_screen.dart
+++ lib/cart_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'component/menu.dart';
 import 'component/store_name.dart';
 
 class CartScreen extends StatefulWidget {
@@ -32,11 +33,17 @@ class _CartScreenState extends State<CartScreen> {
           SizedBox(
             height: 10,
           ),
-          StoreName(name: '치킨 잠실점',),
+          StoreName(
+            name: '치킨 잠실점',
+          ),
           SizedBox(
             height: 1,
           ),
-          _buildMenu(),
+          Menu(
+            name: '후라이드 치킨',
+            description: '• 찜 & 리뷰 약속 : 참여. 서비스음료제공',
+            price: '18,000원',
+          ),
           SizedBox(
             height: 1,
           ),
@@ -95,81 +102,6 @@ class _CartScreenState extends State<CartScreen> {
             ),
           ),
         ),
-      ),
-    );
-  }
-
-  Widget _buildMenu() {
-    return Container(
-      color: Colors.white,
-      child: Column(
-        children: [
-          Row(
-            children: [
-              SizedBox(
-                width: 20,
-              ),
-              Text(
-                '후라이드 치킨',
-                style: TextStyle(
-                  fontSize: 17,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Spacer(),
-              IconButton(
-                icon: Icon(
-                  Icons.close,
-                  color: Colors.grey,
-                ),
-                onPressed: () {},
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              SizedBox(
-                width: 20,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.grey.withOpacity(0.3),
-                    width: 1,
-                  ),
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(12),
-                  child: Image.asset(
-                    'images/chicken.png',
-                    width: 70,
-                    height: 70,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-              SizedBox(
-                width: 10,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '• 찜 & 리뷰 약속 : 참여. 서비스음료제공',
-                    style: TextStyle(
-                      color: Color.fromRGBO(125, 125, 125, 1.0),
-                    ),
-                  ),
-                  Text('18,000원'),
-                ],
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 20,
-          ),
-        ],
       ),
     );
   }
